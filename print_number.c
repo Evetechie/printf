f45123aa3546e19c3cb03d71d12e506c87d71f0b
obaraelijah <elijahobara357@gmail.com>
2022-10-23 06:52:15 +0000

    Added test files and other files

--- print_number.c
+++ print_number.c
@@ -7,32 +7,60 @@
  */
 int print_number(va_list args)
 {
-int n;
-int div;
-int len;
-unsigned int num;
+	int n;
+	int div;
+	int len;
+	unsigned int num;
 
-n  = va_arg(args, int);
-div = 1;
-len = 0;
+	n  = va_arg(args, int);
+	div = 1;
+	len = 0;
 
-if (n < 0)
-{
-len += _write_char('-');
-num = n * -1;
+	if (n < 0)
+	{
+		len += _write_char('-');
+		num = n * -1;
+	}
+	else
+		num = n;
+
+	for (; num / div > 9; )
+		div *= 10;
+
+	for (; div != 0; )
+	{
+		len += _write_char('0' + num / div);
+		num %= div;
+		div /= 10;
+	}
+
+	return (len);
 }
-else
-num = n;
-
-for (; num / div > 9; )
-div *= 10;
-
-for (; div != 0; )
+/**
+ * print_unsgined_number - Prints an unsigned number
+ * @n: unsigned integer to be printed
+ * Return: The amount of numbers printed
+ */
+int print_unsgined_number(unsigned int n)
 {
-len += _write_char('0' + num / div);
-num %= div;
-div /= 10;
-}
+	int div;
+	int len;
+	unsigned int num;
 
-return (len);
+	div = 1;
+	len = 0;
+
+	num = n;
+
+	for (; num / div > 9; )
+		div *= 10;
+
+	for (; div != 0; )
+	{
+		len += _write_char('0' + num / div);
+		num %= div;
+		div /= 10;
+	}
+
+	return (len);
 }
